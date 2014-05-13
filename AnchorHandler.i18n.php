e0cc5e10b9a147cf3e4d6177a309b6899f6b3bda
Ike Hecht <tosfos@yahoo.com>
2014-05-12 21:24:13 -0700

    Initial code
    Change-Id: I4b7d59f952d7b3a60387aa3b83dd3cb092e64a07

---
+++ AnchorHandler.i18n.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Internationalisation for the AnchorHandler extension
+ *
+ * @file
+ * @ingroup Extensions
+ */
+$messages = array();
+
+/** English
+ * @author Ike Hecht
+ */
+$messages[ 'en' ] = array(
+	'anchorhandler-desc' => 'Allows inserting <nowiki><</nowiki>a> anchor tags into wikitext',
+);
+
+/** Message documentation
+ * @author Ike Hecht
+ */
+$messages[ 'qqq' ] = array(
+	'anchorhandler-desc' => '{{desc}}',
+);
---
+++ AnchorHandler.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * AnchorHandler - allows inserting <a> anchor tags into wikitext
+ *
+ * To activate this extension, add the following into your LocalSettings.php file:
+ * require_once('$IP/AnchorHandler/AnchorHandler.php');
+ * $egAnchorNamespaces = array(); //Must be set!
+ *
+ * @ingroup Extensions
+ * @author Ike Hecht
+ * @version 0.1
+ * @link https://www.mediawiki.org/wiki/Extension:AnchorHandler Documentation
+ * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
+ */
+
+if( !defined( 'MEDIAWIKI' ) ) {
+        echo( "This is an extension to the MediaWiki package and cannot be run standalone.\n" );
+        die( -1 );
+}
+
+// Extension credits that will show up on Special:Version
+$wgExtensionCredits['parserhook'][] = array(
+        'path'           => __FILE__,
+        'name'           => 'AnchorHandler',
+        'version'        => '0.1',
+        'author'         => 'Ike Hecht for [http://www.wikiworks.com/ WikiWorks]',
+        'url'            => 'https://www.mediawiki.org/wiki/Extension:AnchorHandler',
+        'descriptionmsg' => 'anchorhandler-desc',
+);
+
+$wgExtensionMessagesFiles[ 'AnchorHandler' ] = __DIR__ . '/AnchorHandler.i18n.php';
+
+$egAnchorNamespaces = array();
+
+$wgHooks['ParserFirstCallInit'][] = 'addAnchorHandler';
+function addAnchorHandler( Parser &$parser ) {
+	$parser->setHook( 'a', 'anchorHandler' );
+
+	return true;
+}
+
+// If current namespace is an "anchor namespace" specified in $egAnchorNamespaces then send out real HTML
+// otherwise, just send out the escaped anchor text
+function anchorHandler( $text, array $args, Parser $parser, PPFrame $frame ) {
+	global $egAnchorNamespaces;
+
+	$namespace = $frame->getTitle()->getNamespace();
+
+	if ( !in_array( $namespace, $egAnchorNamespaces ) ) {
+		return htmlspecialchars( $text );
+	}
+
+	$href = $args['href'];
+	unset ( $args['href'] );
+
+	$output = Linker::makeExternalLink( $href, $text, false, '', $args );
+
+	return $output;
+}
