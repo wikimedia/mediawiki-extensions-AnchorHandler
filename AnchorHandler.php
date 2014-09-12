72ff3189a6b203dca8d7a7faa968c25878671535
Ike Hecht <tosfos@yahoo.com>
2014-09-11 20:43:20 -0700

    fix bug disallowing href
    Change-Id: Ia37eb497fca8b37f62811f1c934d80aaf6358e3c

--- AnchorHandler.php
+++ AnchorHandler.php
@@ -12,20 +12,19 @@
  * @link https://www.mediawiki.org/wiki/Extension:AnchorHandler Documentation
  * @license http://www.gnu.org/copyleft/gpl.html GNU General Public License 2.0 or later
  */
-
-if( !defined( 'MEDIAWIKI' ) ) {
-        echo( "This is an extension to the MediaWiki package and cannot be run standalone.\n" );
-        die( -1 );
+if ( !defined( 'MEDIAWIKI' ) ) {
+	echo ( "This is an extension to the MediaWiki package and cannot be run standalone.\n" );
+	die( -1 );
 }
 
 // Extension credits that will show up on Special:Version
 $wgExtensionCredits['parserhook'][] = array(
-        'path'           => __FILE__,
-        'name'           => 'AnchorHandler',
-        'version'        => '0.1',
-        'author'         => 'Ike Hecht for [http://www.wikiworks.com/ WikiWorks]',
-        'url'            => 'https://www.mediawiki.org/wiki/Extension:AnchorHandler',
-        'descriptionmsg' => 'anchorhandler-desc',
+	'path' => __FILE__,
+	'name' => 'AnchorHandler',
+	'version' => '0.2',
+	'author' => 'Ike Hecht for [http://www.wikiworks.com/ WikiWorks]',
+	'url' => 'https://www.mediawiki.org/wiki/Extension:AnchorHandler',
+	'descriptionmsg' => 'anchorhandler-desc',
 );
 
 $wgExtensionMessagesFiles['AnchorHandler'] = __DIR__ . '/AnchorHandler.i18n.php';
@@ -34,14 +33,15 @@ $wgMessageDirs['AnchorHandler'] = __DIR__ . '/i18n';
 $egAnchorNamespaces = array();
 
 $wgHooks['ParserFirstCallInit'][] = 'addAnchorHandler';
+
 function addAnchorHandler( Parser &$parser ) {
 	$parser->setHook( 'a', 'anchorHandler' );
 
 	return true;
 }
 
-// If current namespace is an "anchor namespace" specified in $egAnchorNamespaces then send out real HTML
-// otherwise, just send out the escaped anchor text
+// If current namespace is an "anchor namespace" specified in $egAnchorNamespaces then send out real
+// HTML. Otherwise, just send out the escaped anchor text.
 function anchorHandler( $text, array $args, Parser $parser, PPFrame $frame ) {
 	global $egAnchorNamespaces;
 
@@ -52,9 +52,8 @@ function anchorHandler( $text, array $args, Parser $parser, PPFrame $frame ) {
 	}
 
 	$href = $args['href'];
-	unset ( $args['href'] );
-
 	$output = Linker::makeExternalLink( $href, $text, false, '', $args );
+	unset( $args['href'] );
 
 	return $output;
 }
