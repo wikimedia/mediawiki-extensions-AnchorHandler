008893a52b5165d15481783fe131681affcde8b3
MarcoAurelio <maurelio@tools.wmflabs.org>
2018-02-14 18:36:41 +0100

    Add COPYING and proper copyright headers
    Change-Id: I1d7209fa998cf8bed211b6ff47da20ff42b2337d

--- AnchorHandler.php
+++ AnchorHandler.php
@@ -1,10 +1,26 @@
 <?php
 /**
- * AnchorHandler - allows inserting <a> anchor tags into wikitext
+ * MediaWiki extension AnchorHandler.
+ * Allows inserting <a> anchor tags into wikitext
  *
  * To activate this extension, add the following into your LocalSettings.php file:
  * require_once('$IP/AnchorHandler/AnchorHandler.php');
  * $egAnchorNamespaces = array(); //Must be set!
+ *
+ * Copyright (C) 2014, Ike Hecht
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
  * @ingroup Extensions
  * @author Ike Hecht
